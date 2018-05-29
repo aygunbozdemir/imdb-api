743276e0844707216e39bfaac431005b389e354f
aygun.bozdemir <aygun.bozdemir@altpro.com.tr>
2018-05-29 10:13:02 +0300

    some modifications in project stracture

--- src/main/java/tr/com/altpro/imdb/boundary/ImdbResource.java
+++ src/main/java/tr/com/altpro/imdb/boundary/ImdbResource.java
@@ -16,22 +16,21 @@ public class ImdbResource {
 
     @CrossOrigin(origins = "http://127.0.0.1:5500")
     @RequestMapping(value = "/getByName",method = RequestMethod.GET)
-    public ResponseEntity<ImdbResponse> getByName(@RequestParam(value = "name",required = true)String name) {
+    public ImdbResponse getByName(@RequestParam(value = "name",required = true)String name) {
 
-        ResponseEntity<ImdbResponse> responseEntity=imdbService.getByName(name);
+
         if (name != null && !name.isEmpty()) {
-            return responseEntity;
+            return imdbService.getByName(name);
         }
         return null;
     }
 
     @CrossOrigin(origins = "http://127.0.0.1:5500")
     @RequestMapping(value = "/getById",method = RequestMethod.GET)
-    public ResponseEntity<ImdbResponse> getById(@RequestParam(value = "id",required = true)String id) {
+    public ImdbResponse getById(@RequestParam(value = "id",required = true)String id) {
 
-        ResponseEntity<ImdbResponse> responseEntity=imdbService.getById(id);
         if (id != null && !id.isEmpty()) {
-            return responseEntity;
+            return imdbService.getById(id);
         }
         return null;
     }
